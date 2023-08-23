c4dfec873785eaf31ffea2aa14dcdffe633aad10
Lucas Capdevila <lcapdevila60@gmail.com>
2023-08-23 19:24:41 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//  Hay 2 maneras de instalar Bootstrap:
+
+
+//  1) Importar Librerías
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+//  Archivos de Proyecto
+
+import './css/style.css' 
